1dc4eaf5a8ee7c2002ab513f70aa38afe53b52eb
Geoff <gnvngo@gmail.com>
2023-04-15 23:54:56 -0400

    add lazy approach to bids conversion for bruker zip files

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """Run tar-to-bids"""
 import argparse, os, sys
+from pathlib import Path
 
 from src.heuristics.utils import HEURISTIC_DIR
-from src.cfmm_tar.cfmm_tar import cfmm_tar
+from src.cfmm_parser.cfmm_tar import cfmm_tar
+from src.cfmm_parser.cfmm_zip import cfmm_zip
 from src.parser.setup_parser import setup_parser
 from src.csv_reader.tar_mapper import task_mapper
 
@@ -16,6 +18,19 @@ def call_heudiconv(
 
     import subprocess
     cmd = f"heudiconv -d {tar_dir}/*/*/*/{{subject}}_{{session}}/*/*/*.dcm -o {out_dir} -f {heuristic_py} -s {subject} -ss {session} -c dcm2niix -b --minmeta --overwrite"
+    output = f"{out_dir}/log.txt"
+    subprocess.run(cmd,shell=True)
+
+def call_heudiconv_zip(
+        tar_dir,
+        out_dir,
+        heuristic_py,
+        subject,
+        session
+    ):
+
+    import subprocess
+    cmd = f"heudiconv -d {tar_dir}/*/*/{{subject}}_{{session}}/*/*/*.dcm -o {out_dir} -f {heuristic_py} -s {subject} -ss {session} -c dcm2niix -b --minmeta --overwrite"
     output = f"{out_dir}/log.txt"
     subprocess.run(cmd,shell=True)
 
@@ -34,7 +49,8 @@ def main():
     args=parser.parse_args()
 
     # Make bids directory, if it does not exist
-    if not os.path.isdir(args.output_dir): os.mkdir(args.output_dir)
+    output_dir = Path(args.output_dir)
+    if not output_dir.exists(): output_dir.mkdir(parents=True)
 
     # Check if session is already run
     subject_session_dir = f"{args.output_dir}/sub-{args.subject}/ses-{args.session}"
@@ -52,29 +68,44 @@ def main():
             sys.exit(0)
 
     # Extract tar file
-    tar_obj = cfmm_tar(args.tar)
-    tar_obj.extract()
+    tar_path = Path(args.tar)
+    file_extension = tar_path.suffix
     '''
     Relabel dicom directory tree with `reformat`. This allows heudiconv to more easily
     read {subject} and {session} info enabling robust bids labeling of each tar file.
     The tradeoff is that `tar-to-bids` requires specifying `subject` and `session`
     with argparse.
     '''
-    reformat = f"{args.subject}_{args.session}" # Relabel dicom 
-    tar_obj.reformat_dicom_tree(reformat)
+    if file_extension == ".tar":
+        tar_obj = cfmm_tar(args.tar)
+        tar_obj.extract()
+        reformat = f"{args.subject}_{args.session}" # Relabel dicom 
+        tar_obj.reformat_dicom_tree(reformat)
+        # heudiconv
+        heuristic = os.path.join(HEURISTIC_DIR,args.heuristic)
+        assert Path(heuristic).exists(), f"{heuristic} does not exist."
+        call_heudiconv(tar_obj.tar_dir,args.output_dir,heuristic,args.subject,args.session)
+        # heudiconv post-processing
+        if run_post_processing:
+            if args.task_mappings is not None: post_process.heudiconv_post_process(tar_obj.tar_tree,args.subject,args.session,args.output_dir,args.heuristic,args.task_mappings)
+            else: post_process.heudiconv_post_process(tar_obj.tar_tree,args.subject,args.session,args.output_dir,args.heuristic,"")
+        # cleanup
+        tar_obj.cleanup()
 
-    # heudiconv
-    heuristic = os.path.join(HEURISTIC_DIR,args.heuristic)
-    assert os.path.exists(heuristic), f"{heuristic} does not exist."
-    call_heudiconv(tar_obj.tar_dir,args.output_dir,heuristic,args.subject,args.session)
-
-    # heudiconv post-processing
-    if run_post_processing:
-        if args.task_mappings is not None: post_process.heudiconv_post_process(tar_obj.tar_tree,args.subject,args.session,args.output_dir,args.heuristic,args.task_mappings)
-        else: post_process.heudiconv_post_process(tar_obj.tar_tree,args.subject,args.session,args.output_dir,args.heuristic,"")
-
-    # cleanup
-    tar_obj.cleanup()
+    elif file_extension == ".zip":
+        zip_obj = cfmm_zip(args.tar)
+        zip_obj.extract()
+        reformat = f"{args.subject}_{args.session}" # Relabel dicom 
+        zip_obj.reformat_dicom_tree(reformat)
+        # heudiconv
+        heuristic = os.path.join(HEURISTIC_DIR,args.heuristic)
+        assert Path(heuristic).exists(), f"{heuristic} does not exist."
+        call_heudiconv_zip(zip_obj.zip_dir,args.output_dir,heuristic,args.subject,args.session)
+        # cleanup
+        zip_obj.cleanup()
+        
+    else:
+        NotImplemented
 
 if __name__ == "__main__":
     main()
