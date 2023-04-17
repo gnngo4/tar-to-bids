2d14502fa71b6f1df649231567c67b0448b2ae4e
Geoff <gnvngo@gmail.com>
2023-04-17 11:54:39 -0400

    change task labels

--- src/heuristics/cfmm_filters/bruker/osc_v1.py
+++ src/heuristics/cfmm_filters/bruker/osc_v1.py
@@ -18,17 +18,13 @@ class bold:
         for s in self.seqinfo:
             description = s.series_description
             if 'task-visualblock_res-A' in description:
-                print('hi')
-                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-VisualBlock_dir-AP_desc-Test01_run-{{item:02d}}_bold')
+                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-TEST01VisualBlock_dir-AP_run-{{item:02d}}_bold')
             elif 'task-visualosc_res-A' in description:
-                print('hi')
-                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-VisualFT_dir-AP_desc-Test01_run-{{item:02d}}_bold')
+                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-TEST01VisualFT_dir-AP_run-{{item:02d}}_bold')
             elif 'task-visualblock_res-B' in description:
-                print('hi')
-                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-VisualBlock_dir-AP_desc-Test02_run-{{item:02d}}_bold')    
+                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-TEST02VisualBlock_dir-AP_run-{{item:02d}}_bold')    
             elif 'task-visualosc_res-B' in description:
-                print('hi')
-                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-VisualFT_dir-AP_desc-Test02_run-{{item:02d}}_bold')    
+                template = create_key(f'sub-{{subject}}/{{session}}/func/sub-{{subject}}_{{session}}_task-TEST02VisualFT_dir-AP_run-{{item:02d}}_bold')    
             else:
                 continue
             
