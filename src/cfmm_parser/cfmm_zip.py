43bd6c722c33e7b4f743152042c8188cb039b647
Geoff <gnvngo@gmail.com>
2023-05-11 13:59:56 -0400

    fix single slice acquisition stackid issue

--- src/cfmm_parser/cfmm_zip.py
+++ src/cfmm_parser/cfmm_zip.py
@@ -4,6 +4,7 @@ import zipfile
 import shutil
 import tempfile
 import pathlib
+import pydicom
 
 
 class cfmm_zip:
@@ -28,7 +29,42 @@ class cfmm_zip:
         shutil.move(common_path, dst)
         print(f"RELABEL: {dst}")
         assert os.path.isdir(dst), f"{dst} does not exist."
+        # Manual retag StackID info for single slice acquisitions
+        _ = [
+            self._rewrite_stackid(dcm_path)
+            for dcm_path in self._get_files_with_suffix(dst, ".dcm")
+        ]
 
     def cleanup(self):
         print(f"REMOVE: {self.zip_dir}")
         shutil.rmtree(self.zip_dir)
+
+    def _get_files_with_suffix(self, directory, suffix):
+        file_list = []
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(suffix):
+                    file_list.append(os.path.join(root, file))
+
+        return file_list
+
+    def _rewrite_stackid(self, dcm_path):
+        file_basename = os.path.basename(dcm_path)
+        file_dir = os.path.dirname(dcm_path)
+
+        ds = pydicom.dcmread(dcm_path)
+        seq = ds.PerFrameFunctionalGroupsSequence
+
+        if not hasattr(
+            ds.PerFrameFunctionalGroupsSequence[0].FrameContentSequence[0], "StackID"
+        ):
+            for frame in seq:
+                frame.FrameContentSequence[0].StackID = "1"
+                frame.FrameContentSequence[0].InstackPositionNumber = 1
+                dim_index_value = frame.FrameContentSequence[0].DimensionIndexValues
+                frame.FrameContentSequence[0].DimensionIndexValues = [
+                    dim_index_value,
+                    1,
+                    1,
+                ]
+            ds.save_as(os.path.join(file_dir, file_basename))
