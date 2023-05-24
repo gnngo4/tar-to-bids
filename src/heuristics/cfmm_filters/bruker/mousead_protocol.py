a8b033e87f0f18d07e73209100870f966e946164
Geoff <gnvngo@gmail.com>
2023-05-24 15:32:30 -0400

    add heuristics for mousead project

---
+++ src/heuristics/cfmm_filters/bruker/mousead_protocol.py
@@ -0,0 +1,49 @@
+"""
+9.4T Bruker GE fast fMRI bold acquisition
+"""
+
+from src.heuristics.cfmm_filters.utils import create_key
+
+
+class protocol:
+    def __init__(self, seqinfo):
+        self.seqinfo = seqinfo
+
+    def get_info(self):
+        # Initialize empty info dict: key=create_key() and value=[] for all images
+        info = {}
+        # Map image key(s) to dicom series_id(s)
+        for s in self.seqinfo:
+            description = s.series_description.lower()
+            if (
+                description.startswith("T2star_rsFMRI".lower())
+                and "_600" in description
+            ):
+                template = create_key(
+                    "sub-{subject}/{session}/func/sub-{subject}_{session}_task-rest_dir-fwd_run-{item:02d}_bold"
+                )
+            elif description.startswith(
+                "T2star_rsFMRI".lower()
+            ) and description.endswith("SAT".lower()):
+                template = create_key(
+                    "sub-{subject}/{session}/fmap/sub-{subject}_{session}_dir-fwd_desc-GE_run-{item:02d}_epi"
+                )
+            elif description.startswith(
+                "T2star_rsFMRI".lower()
+            ) and description.endswith("SAT_RV".lower()):
+                template = create_key(
+                    "sub-{subject}/{session}/fmap/sub-{subject}_{session}_dir-rev_desc-GE_run-{item:02d}_epi"
+                )
+            elif description.startswith("T2_".lower()):
+                template = create_key(
+                    "sub-{subject}/{session}/anat/sub-{subject}_{session}_run-{item:02d}_T2w"
+                )
+            else:
+                continue
+
+            try:
+                info[template].append({"item": s.series_id})
+            except:
+                info[template] = [{"item": s.series_id}]
+
+        return info
---
+++ src/heuristics/prado_mousead_bruker.py
@@ -0,0 +1,30 @@
+"""
+Template heudiconv file from
+https://neuroimaging-core-docs.readthedocs.io/en/latest/pages/heudiconv.html
+"""
+
+
+def infotodict(seqinfo):
+    """Heuristic evaluator for determining which runs belong where
+
+    allowed template fields - follow python string module:
+
+    item: index within category
+    subject: participant id
+    seqitem: run number during scanning
+    subindex: sub index within group
+    """
+
+    from src.heuristics.cfmm_filters.bruker.mousead_protocol import protocol
+
+    # Instantiate all cfmm_filters
+    infos = [
+        protocol(seqinfo).get_info(),
+    ]
+
+    # Load all filters into `info`
+    info = {}
+    for _info in infos:
+        info.update(_info)
+
+    return info
